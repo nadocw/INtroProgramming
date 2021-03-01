8a218396e8475cb189053fafe0d597fceff1e621
Jason <jvance@verogen.com>
2021-03-01 00:25:39 -0800

    Feedback.

--- #4_Wertz_Connor.py
+++ #4_Wertz_Connor.py
@@ -1,25 +1,20 @@
 # Homework #4, Wertz, Connor
-import showFood
-# foodOrder is our list, we can add to it, print it, remove things, whatever we wanna do
-foodOrder = []
 
-test = "" #this is a part of the loop
+# Order is our list, we can add to it, print it, remove things, whatever we wanna do
+import Order
 
-while not test: #basically just means while true
-    response = input("How would you like to change your order? (A)dd, (S)how, (R)emove, or e(X)it: ")
-    action = response[0] # only look at the first letter of what the user inputs
+while True:
+    userInput = input("How would you like to change your order? (A)dd, (S)how, (R)emove, or e(X)it: ")
+    action = userInput[0]
 
     if action == "a" or action == "A":
-        print("Add to my food order")
-        showFood.addSomeFood( foodOrder )
+        Order.add()
     elif action == "s" or action == "S":
-        print("Show orders")
-        showFood.showMyFood( foodOrder )
+        Order.show()
     elif action == "r" or action == "R":
-        print("Remove an item")
-        showFood.removeFood( foodOrder )
+        Order.remove()
     elif action == "x" or action == "X" or action == "e" or action == "E":
         print("Exit program")
-        test = "Done"
+        exit()
     else:
         print("That's not a valid response.")
